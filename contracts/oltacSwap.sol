287444ace2e3bb048d9eb0782f789254cbb5e457
godtide <ayoadebabatide@gmail.com>
2021-11-23 23:51:48 +0100

    onlyowner

--- contracts/oltacSwap.sol
+++ contracts/oltacSwap.sol
@@ -16,6 +16,7 @@ contract OltSwap is PriceConsumerV3 {
 
     address public constant olt = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address Owner;
 
     // For this example, we will set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
@@ -24,14 +25,22 @@ contract OltSwap is PriceConsumerV3 {
 
 
      int256 oracleprice;
+     int8 counter;
 
 
     constructor(ISwapRouter _swapRouter) {
         swapRouter = _swapRouter;
+        msg.sender == Owner;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == Owner, "not owner");
+        require(counter == 0, "can only be called once");
+        _;
     }
 
 
-    function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
+    function swapExactInputSingle(uint256 amountIn)  onlyOwner() external returns (uint256 amountOut)  {
         // msg.sender must approve this contract
 
         // Transfer the specified amount of olt to this contract.
@@ -40,7 +49,7 @@ contract OltSwap is PriceConsumerV3 {
         // Approve the router to spend olt.
         TransferHelper.safeApprove(olt, address(swapRouter), amountIn);
 
-        oracleprice =  this.getLatestPrice(); 
+        oracleprice =  getLatestPrice(); 
 
         uint divider = uint256(oracleprice/ (10 ** 6) / (4500 * 10 ** 8));
 
